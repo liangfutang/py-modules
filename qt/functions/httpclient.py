44588a593c84c58877a5785cff2dfec6877774d3
liangfutang <tanglf@kalman-navigation.com>
2024-11-10 13:23:35 +0800

    1. 按钮操作postman的header增减

--- qt/functions/httpclient.py
+++ qt/functions/httpclient.py
@@ -11,6 +11,34 @@ def resource_path(relative_path):
         base_path = os.path.abspath(".")
     return os.path.join(base_path, relative_path)
 
+class ButtonOperate:
+    def __init__(self, ui):
+        self.ui = ui
+        # 主界面的所有按钮
+        self.handlerButtton()
+
+    def handlerButtton(self):
+        # header操作
+        self.headerHandler()
+
+    def headerHandler(self):
+        def addHeader():
+            # 在表格末尾添加一行
+            rowPosition = self.ui.headTable.rowCount()
+            self.ui.headTable.insertRow(rowPosition)
+        def subHeader():
+            # 删除选中的行，如果没有选中行则删除最后一行
+            selected_rows = self.ui.headTable.selectionModel().selectedIndexes()
+            if selected_rows:
+                for index in reversed(selected_rows):
+                    self.ui.headTable.removeRow(index.row())
+            else:
+                rowPosition = self.ui.headTable.rowCount()
+                self.ui.headTable.removeRow(rowPosition - 1)
+
+        self.ui.headAddButton.clicked.connect(addHeader)
+        self.ui.headSubButton.clicked.connect(subHeader)
+
 class Stats:
 
     def __init__(self):
@@ -19,6 +47,8 @@ class Stats:
         ui_file.open(QFile.ReadOnly)
         self.ui = QUiLoader().load(ui_file)
         ui_file.close()
+        # 按钮的信号和槽
+        ButtonOperate(self.ui)
 
 app = QApplication([])
 stats = Stats()
